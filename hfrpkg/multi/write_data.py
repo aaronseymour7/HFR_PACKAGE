08e979f872f055e64bcfb670b339cd4e4be027cf
aaronseymour7 <ads09449@uga.edu>
2025-07-18 12:46:38 -0400

    Update write_data.py

--- hfrpkg/multi/write_data.py
+++ hfrpkg/multi/write_data.py
@@ -3,7 +3,11 @@ import glob
 import re
 from hfrpkg.multi.compute_folder import compute_folder
 
-
+def kjTokcal(value):
+        try:
+            return value / 4.184
+        except (TypeError, ZeroDivisionError):
+            return None  
 def write_single_reaction(reaction_data, folder_path):
     output_path = os.path.join(folder_path, "reaction_summary.txt")
     with open(output_path, "w", encoding="utf-8") as fout:
@@ -18,4 +22,13 @@ def write_single_reaction(reaction_data, folder_path):
             fout.write(f"{coeff} {smiles}\t{inchi}\t{atct}\n")
         
         fout.write("\n")
-    return reaction_data['input_smiles'],reaction_data['level'],reaction_data['input_inchi'],reaction_data['dft_hf'],reaction_data['input_atct']
+    
+
+    return (
+        reaction_data['input_smiles'],
+        reaction_data['level'],
+        reaction_data['input_inchi'],
+        kjTokcal(reaction_data['dft_hf']),
+        kjTokcal(reaction_data['input_atct']),
+    )
+    #return reaction_data['input_smiles'],reaction_data['level'],reaction_data['input_inchi'],reaction_data['dft_hf']/4.184,reaction_data['input_atct']/4.184
