80e6d726dd217aa154623f39c3194e8446a7653f
aaronseymour7 <ads09449@uga.edu>
2025-08-05 11:05:55 -0700

    Update run_spec.py
    added ext cli

--- hfrpkg/run_spec.py
+++ hfrpkg/run_spec.py
@@ -10,11 +10,19 @@ from AaronTools.job_control import SubmitProcess
 from AaronTools.theory.job_types import SinglePointJob
 from hfrpkg.run_single import run_jobs
 
-def make_spec(method, basis):
-
+def make_spec(method, basis, extension):
+    
     folder = os.getcwd()
     log_files = glob.glob(os.path.join(folder, "*.log"))
-
+    extension_map = {
+        "g": ".com",
+        "o": ".inp",
+        "p": ".in"
+    }
+    ext = extension_map.get(extension.lower())
+    if ext is None:
+        print(f"Unknown software code '{extension}'. Please use 'g', 'o', or 'p'.")
+        sys.exit(1)
     if not log_files:
         print("No .log files found in folder.")
         
@@ -29,7 +37,7 @@ def make_spec(method, basis):
 
     for log_path in log_files:
         name = os.path.splitext(os.path.basename(log_path))[0]
-        com_path = os.path.join(folder, name + ".com")
+        com_path = os.path.join(folder, name + ext)
 
         try:
             geom = Geometry(log_path)
@@ -45,9 +53,13 @@ def main_cli():
     parser = argparse.ArgumentParser(description="Generate and submit SP .com files from optimized .log files")
     parser.add_argument("--m", "--method", dest="method", default="m06-2x", help="DFT method (default: m06-2x)")
     parser.add_argument("--b", "--basis", dest="basis", default="def2tzvp", help="Basis set (default: def2tzvp)")
+    parser.add_argument(
+        "--s", choices=["g", "o", "p"], default="g",
+        help="Software to use: 'g' for Gaussian, 'o' for Orca, 'p' for Psi4 (default: 'g')"
+    )
     args = parser.parse_args()
 
-    make_spec(method=args.method, basis=args.basis)
+    make_spec(method=args.method, basis=args.basis, extension = args.s)
 
 if __name__ == "__main__":
     main_cli()
