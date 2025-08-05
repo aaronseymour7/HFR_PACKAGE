2eea850257701f43e24b2a887ad956f1bf41e5be
aaronseymour7 <ads09449@uga.edu>
2025-08-05 11:01:20 -0700

    Update unique_spec.py

--- hfrpkg/multi/unique_spec.py
+++ hfrpkg/multi/unique_spec.py
@@ -9,7 +9,16 @@ from AaronTools.theory.job_types import SinglePointJob
 from hfrpkg.run_single import run_jobs
 
 
-def make_spec(method, basis):
+def make_spec(method, basis, extension):
+    extension_map = {
+    "g": ".com",
+    "o": ".inp",
+    "p": ".in"
+    }
+    ext = extension_map.get(extension.lower())
+    if ext is None:
+        print(f"Unknown software code '{extension}'. Please use 'g', 'o', or 'p'.")
+        sys.exit(1)
     folder = "unique_com_files/"
     log_files = glob.glob(os.path.join(folder, "*.log"))
 
@@ -24,10 +33,10 @@ def make_spec(method, basis):
         job_type=SinglePointJob()
     )
     com_files = []
-
+    
     for log_path in log_files:
         name = os.path.splitext(os.path.basename(log_path))[0]
-        com_path = os.path.join(folder, name + ".com")
+        com_path = os.path.join(folder, name + ext)
 
         try:
             geom = Geometry(log_path)
@@ -42,10 +51,13 @@ def main_cli():
     parser = argparse.ArgumentParser(description="Generate and submit SP .com files from optimized .log files")
     parser.add_argument("--m", "--method", dest="method", default="m06-2x", help="DFT method (default: m06-2x)")
     parser.add_argument("--b", "--basis", dest="basis", default="def2tzvp", help="Basis set (default: def2tzvp)")
-    parser.add_argument("--s", default="g", help="Software to use ['g'-Gaussian, 'o'-Orca, 'p'-Psi4] (default:Gaussian)")
+    parser.add_argument(
+        "--s", choices=["g", "o", "p"], default="g",
+        help="Software to use: 'g' for Gaussian, 'o' for Orca, 'p' for Psi4 (default: 'g')"
+    )
     args = parser.parse_args()
 
-    make_spec(method=args.method, basis=args.basis, ext = s)
+    make_spec(method=args.method, basis=args.basis, extension = args.s)
 
 if __name__ == "__main__":
     main_cli()
