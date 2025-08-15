06383edebeb33186088c686269c723c66e761c65
aaronseymour7 <ads09449@uga.edu>
2025-08-15 14:06:34 -0400

    Update write_data.py
    opt_summary created, energy and zpve added

--- hfrpkg/write_data.py
+++ hfrpkg/write_data.py
@@ -9,18 +9,18 @@ def kjTokcal(value):
         except TypeError:
             return None  
 def write_single_reaction(reaction_data, folder_path):
-    output_path = os.path.join(folder_path, "reaction_summary.txt")
+    output_path = os.path.join(folder_path, "opt_summary.txt")
     with open(output_path, "w", encoding="utf-8") as fout:
         fout.write(f"{reaction_data['input_smiles']}\t{reaction_data['input_inchi']}\n")
         fout.write(f"Reaction Enthalpy (kJ/mol):{reaction_data['reaction_H']}\n")
         fout.write(f"DFT Enthalpy of Formation (kJ/mol):{reaction_data['dft_hf']}\n")
         fout.write("REACTANTS\n")
-        for coeff, smiles, inchi, atct in reaction_data['reactants']:
-            fout.write(f"{coeff} {smiles}\t{inchi}\t{atct}\n")
+        for coeff, smiles, inchi, atct, energy, zpve in reaction_data['reactants']:
+            fout.write(f"{coeff} {smiles}\t{inchi}\t{atct}\t{energy}\t{zpve}\n")
         
         fout.write("PRODUCTS\n")
-        for coeff, smiles, inchi, atct in reaction_data['products']:
-            fout.write(f"{coeff} {smiles}\t{inchi}\t{atct}\n")
+        for coeff, smiles, inchi, atct, energy, zpve in reaction_data['products']:
+            fout.write(f"{coeff} {smiles}\t{inchi}\t{atct}\t{energy}\t{zpve}\n")
         
         fout.write("\n")
     
