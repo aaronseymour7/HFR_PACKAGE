d4f56ffcfd12912b3d6664691c4bc4b10cbf3605
aaronseymour7 <ads09449@uga.edu>
2025-08-17 17:08:31 -0400

    Update unique_spec.py

--- hfrpkg/multi/unique_spec.py
+++ hfrpkg/multi/unique_spec.py
@@ -79,17 +79,4 @@ def make_spec(method, basis, extension):
         os.chdir(cwd)
         
 
-def main_cli():
-    parser = argparse.ArgumentParser(description="Generate and submit SP .com files from optimized .log files")
-    parser.add_argument("--m", "--method", dest="method", default="m06-2x", help="DFT method (default: m06-2x)")
-    parser.add_argument("--b", "--basis", dest="basis", default="def2tzvp", help="Basis set (default: def2tzvp)")
-    parser.add_argument(
-        "--s", choices=["g", "o", "p"], default="g",
-        help="Software to use: 'g' for Gaussian, 'o' for Orca, 'p' for Psi4 (default: 'g')"
-    )
-    args = parser.parse_args()
 
-    make_spec(method=args.method, basis=args.basis, extension = args.s)
-
-if __name__ == "__main__":
-    main_cli()
