534603f34ae64c84ca9e2e56da11259c86d273d7
aaronseymour7 <ads09449@uga.edu>
2025-08-19 14:36:08 -0400

    Update grab_unique.py

--- hfrpkg/multi/grab_unique.py
+++ hfrpkg/multi/grab_unique.py
@@ -5,20 +5,20 @@ import shutil
 from hfrpkg.utils import get_extensions
 
 
-def grab_unique_coms():
+def grab_unique_coms(software):
     output_dir = "unique_files"
     os.makedirs(output_dir, exist_ok=True)
 
     seen_inchis = {}
-    counter = 1  # numbering starts at 1
+    counter = 1  
 
     output_index_path = os.path.join(output_dir, "index.txt")
     with open(output_index_path, "w") as index_out:
-        index_out.write("Filename\tInChI\tSMILES\tSoftware\n")
+        index_out.write(f"Software: {software}\n")
+        index_out.write("Filename\tInChI\tSMILES\n")
 
         for mhfr_dir in sorted(glob.glob("*.mhfr")):
             index_file_path = os.path.join(mhfr_dir, "index.txt")
-            software = ""
             if not os.path.isfile(index_file_path):
                 continue
             inext, outext = get_extensions(index_file_path)
@@ -26,16 +26,7 @@ def grab_unique_coms():
                 lines = f.readlines()
 
             for line in lines:
-                if line.startswith("Filename"):
-                    continue
-                if line.startswith("Level:"):           
-                    try:
-                        parts = line.strip().split("\t")
-                        if len(parts) >= 4:
-                            software = parts[3]
-                    except FileNotFoundError:
-                        print(f"No software found in index.txt")
-                        continue
+                if line.startswith("Filename") or line.startswith("Level:"):
                     continue
 
                 parts = line.strip().split("\t")
@@ -44,7 +35,7 @@ def grab_unique_coms():
 
                 filename, inchi, smiles = parts
                 if inchi in seen_inchis:
-                    continue  # already processed
+                    continue  
                 
                 source_com_path = os.path.join(mhfr_dir, filename + inext)
                 if not os.path.isfile(source_com_path):
@@ -56,7 +47,7 @@ def grab_unique_coms():
 
                 shutil.copyfile(source_com_path, dest_com_path)
 
-                index_out.write(f"{counter}\t{inchi}\t{smiles}\t{software}\n")
+                index_out.write(f"{counter}\t{inchi}\t{smiles}\n")
 
                 seen_inchis[inchi] = counter
                 counter += 1
